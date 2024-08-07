9e0ddfd4d0b576c647b6ae3c1bcf86b907a87762
Arnold Liu <forjunks@email.com>
2024-08-07 17:47:40 +0800

    Update timestamp with zone in Util

--- src/main/java/com/crypto/util/SqlUtil.java
+++ src/main/java/com/crypto/util/SqlUtil.java
@@ -3,10 +3,16 @@ package com.crypto.util;
 import org.springframework.stereotype.Component;
 
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 @Component
 public class SqlUtil {
     public Timestamp createCurrentTimestamp() {
-        return new java.sql.Timestamp(System.currentTimeMillis());
+        ZoneId zoneSg = ZoneId.of("Singapore");
+        LocalDateTime now = LocalDateTime.now();
+        ZonedDateTime zonedDateTime = ZonedDateTime.of(now, zoneSg);
+        return Timestamp.from(zonedDateTime.toInstant());
     }
 }
